e590c2af2242da26a5f36477fbc036b273bb9e9d
yongk_liu <1194117884@qq.com>
2018-05-24 15:27:13 +0800

    部分修改

--- src/main/java/com/lf/car/service/HotSliderService.java
+++ src/main/java/com/lf/car/service/HotSliderService.java
@@ -18,28 +18,28 @@ public class HotSliderService {
         List<HotForSlider> list = new ArrayList<>();
 
         HotForSlider slider1 = new HotForSlider();
-        slider1.setImg("http://p6wghqgva.bkt.clouddn.com/FitA9NTg05M3HtCO7TjTtv_t_DQp.jpg");
+        slider1.setImg("http://lf.datangleiyinsi.com/08100334oqpa.jpg");
         slider1.setHref("https://baidu.com");
         slider1.setIntroduction("这个是简介");
         slider1.setTitle("这个是标题");
         list.add(slider1);
 
         HotForSlider slider2 = new HotForSlider();
-        slider2.setImg("http://p6wghqgva.bkt.clouddn.com/FitA9NTg05M3HtCO7TjTtv_t_DQp.jpg");
+        slider2.setImg("http://lf.datangleiyinsi.com/08100334oqpa.jpg");
         slider2.setHref("https://sohu.com");
         slider2.setIntroduction("这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介");
         slider2.setTitle("这个是标题");
         list.add(slider2);
 
         HotForSlider slider3 = new HotForSlider();
-        slider3.setImg("http://p6wghqgva.bkt.clouddn.com/FitA9NTg05M3HtCO7TjTtv_t_DQp.jpg");
+        slider3.setImg("http://lf.datangleiyinsi.com/08100334oqpa.jpg");
         slider3.setHref("https://163.com");
         slider3.setIntroduction("这个是简介");
         slider3.setTitle("这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题");
         list.add(slider3);
 
         HotForSlider slider4 = new HotForSlider();
-        slider4.setImg("http://p6wghqgva.bkt.clouddn.com/FitA9NTg05M3HtCO7TjTtv_t_DQp.jpg");
+        slider4.setImg("http://lf.datangleiyinsi.com/08100334oqpa.jpg");
         slider4.setHref("https://qq.com");
         slider4.setIntroduction("这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介这个是简介");
         slider4.setTitle("这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题这个是标题");
--- src/main/resources/static/front/js/buyService.js
+++ src/main/resources/static/front/js/buyService.js
@@ -17,6 +17,9 @@ $(function () {
     //出发本页clik
     $('.pet_bottom_nav_list li[type="' + thisPage + '"]').click();
 
+    //初始化jsapi
+    initWechatSdk();
+
     //首页轮播s
     getHotImg();
 });
@@ -80,6 +83,70 @@ function getHotImg() {
                 $(".bs_body_slider").html(htmlText);
 
                 $('.am-slider').flexslider();
+            } else {
+
+            }
+        },
+        error: function (error, status) {
+
+        }
+    })
+}
+
+
+function initWechatSdk() {
+    $.ajax({
+        type: "get",
+        url: "/api/wechat/jssdk_config",
+        data: "url=" + location.href,
+        success: function (data, status) {
+            if (data.code == 0) {
+                var config = data.data;
+
+                if (window.wx){
+                    wx.onMenuShareTimeline({
+                        title: '哈哈', // 分享标题
+                        link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: 'http://a.datangleiyinsi.com/IMG_20180417_082558.jpg', // 分享图标
+                        success: function () {
+                            // 用户点击了分享后执行的回调函数
+                        },
+                        fail: function (err) {
+                            console.log(err)
+                        }
+                    });
+
+                    wx.onMenuShareAppMessage({
+                        title: '哈哈', // 分享标题
+                        link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: 'http://lf.datangleiyinsi.com/IMG_20180417_082558.jpg', // 分享图标
+                        type: '', // 分享类型,music、video或link，不填默认为link
+                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                        success: function () {
+                            // 用户点击了分享后执行的回调函数
+                        },
+                        fail: function (err) {
+                            console.log(err)
+                        }
+                    });
+                    /*
+                * 注意：
+                * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
+                * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+                */
+                    wx.config({
+                        beta: true, //注入wx.invoke和wx.on方法。wx.invoke用于调用未开放的由页面主动调用的JS接口，wx.on用于调用未开放的监听类JS接口。
+                        debug: true,
+                        appId: config.appId,
+                        timestamp: config.timestamp,
+                        nonceStr: config.nonceStr,
+                        signature: config.signature,
+                        jsApiList: [
+                            'onMenuShareTimeline',
+                            'onMenuShareAppMessage'
+                        ]
+                    });
+                }
             } else {
 
             }
